1348a3234df23709feeacdcc1c0fe7db6dd4d67d
HuiLanAi <310-we-aaa-1@163.com>
2020-08-17 09:19:49 +0800

    basic material， 已经收到了一封拒信和一个修改意见

---
+++ gauss.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+
+mu ,sigma = 0, 1
+sampleNo = 1000000
+np.random.seed(0)
+s = np.random.normal(mu, sigma, sampleNo)
+
+plt.hist(s, bins=100, normed=True, color = "grey")
+plt.show()
